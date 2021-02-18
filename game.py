21906e5e8ae683944ac7f7e7cd62e0906214d0a8
harshvardhan2707 <72443636+harshvardhan2707@users.noreply.github.com>
2021-02-19 01:00:18 +0530

    Update game.py

--- game.py
+++ game.py
@@ -15,7 +15,7 @@ dis_height = 400
  
 dis = pygame.display.set_mode((dis_width, dis_height))
 pygame.display.set_caption('Snake Game')
-pygame.mixer.music.load("C:\\Users\\lenovo\\OneDrive\\Desktop\\CSE Project\\Space_Invaders\\background.wav")
+pygame.mixer.music.load("SpaceInvaders\\background.wav")
 pygame.mixer.music.play(-1)
  
 clock = pygame.time.Clock()
